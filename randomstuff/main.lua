3bcf7aa2f85a031fea2ee382e5b5354071f089cc
mathoshi <47573361+mathoshi@users.noreply.github.com>
2019-04-17 12:56:50 -0400

    Update main.lua

--- randomstuff/main.lua
+++ randomstuff/main.lua
@@ -6,19 +6,31 @@
 -- This will implement a d-pad to move a sprite
 -----------------------------------------------------------------------------------------
 
-local rightArrow = display.newImageRect( "assets/luxoball.png")
+local background = display.setDefault( "background", )
+
+local rightRect = display.newRoundedRect( 270, 480, 80, 60, 10)
+rightRect:setFillColor( 1, 1, 1)
+rightRect.alpha = 0.5
+
+local luxoball = display.newImageRect( "assets/luxoball.png", 100, 98)
+luxoball.x = display.contentCenterX
+luxoball.y = 400
+
+local rightArrow = display.newImageRect( "assets/arrow.png", 60, 60)
+rightArrow.x = 270
+rightArrow.y = 480
 
 function rightArrow:touch( event )
-    if ( event.phase == "moved" ) then
+    if ( event.phase == "moved" or event.phase == "ended" ) then
         -- move the character up
-        transition.moveBy( theCharacter, { 
-        	x = 0, -- move 0 in the x direction 
-        	y = -50, -- move up 50 pixels
-        	time = 100 -- move in a 1/10 of a second
+        transition.moveBy( luxoball, { 
+        	x = 25, -- move 0 in the x direction 
+        	y = 0, -- move up 50 pixels
+        	time = 1 -- move in a 1/10 of a second
         	} )
     end
 
     return true
 end
 
-upArrow:addEventListener( "touch", rightArrow )
+rightArrow:addEventListener( "touch", rightArrow )
